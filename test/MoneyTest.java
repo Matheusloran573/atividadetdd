06f13e169419344df82bb9bd07889f078cd36df1
matheusloran573 <matheusloran573@gmail.com>
2025-03-27 04:39:54 -0300

    "Teste inicial falhando: times() não implementado"

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -6,6 +6,9 @@ public class MoneyTest {
     static void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
-        assert five.amount == 10 : "Erro: 5 * 2 deveria ser 10";
+        if (five.amount != 10) {
+            throw new AssertionError("Falha no teste: 5 * 2 deveria ser 10");
+        }
+        System.out.println("Todos os testes passaram!");
     }
 }
