1554188397bcd0e134fd3186c45a588df4b25573
matheusloran573 <matheusloran573@gmail.com>
2025-03-27 19:47:38 -0300

    3 etapa

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,13 +1,9 @@
-
-     int amount = 10;
-     Dollar(int amount) {}
-     void times(int multiplier) {}
- }
-     int amount;
-     Dollar(int amount) {
+   Dollar(int amount) {
          this.amount = amount;
      }
      void times(int multiplier) {
          amount = amount * multiplier;
+     Dollar times(int multiplier) {
+         return new Dollar(amount * multiplier);
      }
  }
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -1,11 +1,10 @@
-import org.junit.Test;
- import static org.junit.Assert.*;
- 
- public class MoneyTest {
-     @Test
+@Test
      public void testMultiplication() {
          Dollar five = new Dollar(5);
          five.times(2);
          assertEquals(10, five.amount);
+         assertEquals(new Dollar(10), five.times(2));
+         assertEquals(new Dollar(15), five.times(3));
      }
  }
+}
